adf47d2b35c170a3be055dfb623805286b62fc99
heavy_stone <>
2019-10-09 22:10:11 +0900

    "Implement basic login"

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -36,5 +36,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert flash[:success]
     assert_select 'div.alert, .alert-success', 'Welcome to the Sample App!'
+    assert is_logged_in?
   end
 end
