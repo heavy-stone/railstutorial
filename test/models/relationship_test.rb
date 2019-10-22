3cfc7c07dc6b9b38c595cb1f657813175e772423
heavy_stone <>
2019-10-22 22:28:05 +0900

    "Add user following"

---
+++ test/models/relationship_test.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'test_helper'
+
+class RelationshipTest < ActiveSupport::TestCase
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:michael).id,
+                                     followed_id: users(:archer).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
+end
